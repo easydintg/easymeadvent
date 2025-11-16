4a2d51ef8fbb1ce0164c63785bfa4b0f5efff1bd
easydintg <anastasia.sevastova@yandex.ru>
2025-11-16 15:54:46 +0300

    Delete Новогодний адвент календарь/src directory

--- Новогодний адвент календарь/src/public/telegram-webapp.js
+++
@@ -1,3 +0,0 @@
-// This file is loaded from Telegram's CDN
-// Script URL: https://telegram.org/js/telegram-web-app.js
-// This is a placeholder - the actual script will be loaded from Telegram's servers
