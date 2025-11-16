c5460aa89abbc6c3674639a283e497f1fec4d0b7
easydintg <anastasia.sevastova@yandex.ru>
2025-11-16 16:56:43 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,10 @@
-alert("КОД ЗАГРУЗИЛСЯ");
-console.log("MAIN LOADED");
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-
-// === Добавлено для проверки Telegram WebApp ===
-const tg = (window as any).Telegram?.WebApp;
-
-if (!tg) {
-  alert("Telegram WebApp не найден. Вы открыли приложение НЕ через Telegram.");
-} else {
-  const data = tg.initDataUnsafe;
-  alert("Telegram initDataUnsafe:\n\n" + JSON.stringify(data, null, 2));
-  console.log("Telegram initDataUnsafe:", data);
-}
-// ==============================================
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
